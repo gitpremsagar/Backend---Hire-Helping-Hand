08ccecf4d78fdcfe4e3552ba893084fae8d4b84f
gitpremsagar <psagar172@gmail.com>
2023-05-16 21:37:17 +0530

    feat - basic realtime chat works

--- routes/chat/chatMessagesHandler.js
+++ routes/chat/chatMessagesHandler.js
@@ -5,20 +5,19 @@ const auth = require("../../middlewares/auth");
 const router = express.Router();
 
 // route handler to get all chat contacts of authorised user
-router.get("/", auth, async (req, res) => {
+router.get("/:activeContactID", auth, async (req, res) => {
   // dont allow user to access contacts if no or invalid token is provided
   console.log(req.user);
+  console.log("req body = ", req.body);
+
   if (req.user === "invalidToken" || req.user === "notLoggedIn")
     return res.status(400).json({ error: "invalid or no token provided" });
 
-  //user is autorized so send her contact list
-  try {
-    // const sql = `SELECT DISTINCT cm.sender_id, cm.recipient_id, u1.first_name AS sender_first_name,u1.last_name AS sender_last_name, u2.first_name AS recipient_first_name,u2.last_name AS recipient_last_name
-    // FROM chat_messages cm
-    // JOIN users u1 ON u1.idusers = cm.sender_id
-    // JOIN users u2 ON u2.idusers = cm.recipient_id
-    // WHERE cm.sender_id = ? OR cm.recipient_id = ?;`;
+  const activeContactID = req.params.activeContactID;
+  const userID = req.user.idusers;
 
+  //user is autorized so send her stored messages
+  try {
     const sql = `SELECT
       cm.id,
       cm.sender_id,
@@ -33,14 +32,15 @@ router.get("/", auth, async (req, res) => {
       JOIN users s ON cm.sender_id = s.idusers
       JOIN users r ON cm.recipient_id = r.idusers
     WHERE
-      cm.sender_id = ? OR cm.recipient_id = ?;`;
+      (cm.sender_id = ? AND cm.recipient_id = ?) OR (cm.sender_id = ? AND cm.recipient_id = ?);`;
 
-    const params = [req.user.idusers, req.user.idusers];
+    const params = [activeContactID, userID, userID, activeContactID];
     const [rows] = await makeQueryToDatabase(
       process.env.MYSQL_DB_NAME,
       sql,
       params
     );
+    console.log("rows = ", rows);
     res.send(rows);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
